357c8b0fa3e6a4ba3861e720788e86c51b6442bd
Chandru Kannan <chandru.kannan@merlinsourcing.com>
2025-06-09 21:32:27 +0530

    Expense Component Introduced - Routing Done

--- src/app/expense/expense.module.ts
+++ src/app/expense/expense.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ExpenseRoutingModule } from './expense-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { RecurringComponent } from './recurring/recurring.component';
+import { StatsComponent } from './stats/stats.component';
+import { SettingsComponent } from './settings/settings.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    RecurringComponent,
+    StatsComponent,
+    SettingsComponent
     
   ],
   imports: [
