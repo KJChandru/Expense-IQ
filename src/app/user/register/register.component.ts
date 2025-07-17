0348a7e6166ca6ddcbcd3d57b43971972bbb7e41
Chandru Kannan <chandru.kannan@merlinsourcing.com>
2025-07-17 22:31:14 +0530

    Validation check and Sidebar css

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,35 +7,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  RegisterForm!:FormGroup;
+  RegisterForm!: FormGroup;
   passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
 
-constructor(private fb:FormBuilder){
-  this.RegisterForm=this.fb.group({
-    id:['0'],
-    username:['',[Validators.required,Validators.minLength(4),Validators.maxLength(20)]],
-    firstName:['',[Validators.required,Validators.required,Validators.minLength(3),Validators.maxLength(20)]],
-    lastName:['',[Validators.required,Validators.required,Validators.minLength(3),Validators.maxLength(20)]],
-    password: ['', [
+  constructor(private fb: FormBuilder) {
+    this.RegisterForm = this.fb.group({
+      id: ['0'],
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      firstName: ['', [Validators.required, Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      lastName: ['', [Validators.required, Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      password: ['', [
         Validators.required,
         Validators.pattern(this.passwordRegex)
       ]],
-  })
-}
-imageupload(event: Event): void {
-  const input = event.target as HTMLInputElement;
-  if (input?.files?.length) {
-    const file = input.files[0];
-    this.RegisterForm.get('profile')?.setValue(file);
-
-    const reader = new FileReader();
-   
-    reader.readAsDataURL(file);
+    })
   }
-}
 
-
-OnCreateUser(){
-console.log(this.RegisterForm.value)
-}
+  OnCreateUser() {
+    console.log(this.RegisterForm.value)
+  }
 }
