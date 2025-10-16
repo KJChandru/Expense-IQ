935c39c90c3aa096b837a9722f89245f425b10e3
KJChandru <ichandrukj@gmail.com>
2025-10-16 21:13:23 +0530

    Loading Spinnner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingService } from './Service/loading.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Expense_Tracker';
+  isLoading: boolean = false;
+  constructor(private loadingService: LoadingService) {}
+
+   ngOnInit() {
+    this.loadingService.loading$.subscribe(status => {
+      this.isLoading = status;
+    });
+  }
 }
